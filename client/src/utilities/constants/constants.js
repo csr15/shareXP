3edf1a2a3d649c34a00f4667ff84bb7508787471
Ragul CS <ragulcs15@gmail.com>
2020-12-11 10:27:39 +0530

    V1.1.12

--- client/src/utilities/constants/constants.js
+++ client/src/utilities/constants/constants.js
@@ -1,7 +1,7 @@
 export const config = {
   imgAlt:
     "shareXP is used to share your experience with others on how you succeed. It can be any experience that might help other people who are struggling towards success!",
-  server_url: "http://localhost:8080/api/v1",
+  server_url: "https://sharexp12.herokuapp.com/api/v1",
   apiKey: "AIzaSyDqqMYPWwyOI-fpfkB9rzejtlEb_h7a4DU",
   authDomain: "sharexp-15.firebaseapp.com",
   databaseURL: "https://sharexp-15.firebaseio.com",
