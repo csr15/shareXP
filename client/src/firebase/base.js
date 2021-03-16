ff0c8eeb2f10b7611185b25af6c670076a429120
Ragul CS <46232883+csr15@users.noreply.github.com>
2021-03-16 17:32:50 +0530

    Updated firebase credentials

--- client/src/firebase/base.js
+++ client/src/firebase/base.js
@@ -3,15 +3,15 @@ import "firebase/storage";
 import "firebase/analytics"
 
 const firebaseConfig = {
-  server_url: "http://localhost:8080",
-  apiKey: "AIzaSyDqqMYPWwyOI-fpfkB9rzejtlEb_h7a4DU",
-  authDomain: "sharexp-15.firebaseapp.com",
-  databaseURL: "https://sharexp-15.firebaseio.com",
-  projectId: "sharexp-15",
-  storageBucket: "sharexp-15.appspot.com",
+  server_url: "XXXXXXXXXXXXXXX",
+  apiKey: "XXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXX",
   messagingSenderId: "659687712689",
-  appId: "1:659687712689:web:65ea5371425e6588d67f90",
-  measurementId: "G-Q3D8TY9HLW",
+  appId: "XXXXXXXXXXXXXXX",
+  measurementId: "XXXXXXXXXXXXXXX",
 };
 
 firebase.initializeApp(firebaseConfig);
